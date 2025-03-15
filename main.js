c67a7d89f23b4da0d06a49ee8f458c1a23242134
electron271 <66094410+electron271@users.noreply.github.com>
2025-03-15 12:31:07 -0500

    accuracy is dead

--- main.js
+++ main.js
@@ -35,3 +35,22 @@ function buybook() {
 
     alert("Book purchased! Check your downloads folder for the PDF!")
 }   
+
+function buyalbum() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "ACCURACY IS DEAD.zip", true);
+    xhr.responseType = "blob";
+    xhr.onload = function() {
+        var urlCreator = window.URL || window.webkitURL;
+        var zipUrl = urlCreator.createObjectURL(this.response);
+        var tag = document.createElement('a');
+        tag.href = zipUrl;
+        tag.download = "ACCURACY IS DEAD.zip";
+        document.body.appendChild(tag);
+        tag.click();
+        document.body.removeChild(tag);
+    }
+    xhr.send();
+
+    alert("Album purchased! Check your downloads folder for the ZIP file!")
+}
